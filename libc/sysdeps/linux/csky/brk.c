a1f655c21a65b85e6c30193f10fc4c658ab83cad
Guo Ren <ren_guo@c-sky.com>
2017-08-10 20:40:36 +0800

    csky: cleanup brk.c bad design.
    Signed-off-by: Guo Ren <ren_guo@c-sky.com>

--- libc/sysdeps/linux/csky/brk.c
+++ libc/sysdeps/linux/csky/brk.c
@@ -1,42 +1,25 @@
-/* consider this code LGPL - davidm */
-
-#ifdef __CSKYABIV2__
-#include "abiv2_brk.c"
-#else
-
 #include <unistd.h>
 #include <sys/syscall.h>
 #include <errno.h>
 
-libc_hidden_proto(brk)
-
-/* This must be initialized data because commons can't have aliases.  */
 void * __curbrk = 0;
 
 int brk (void *addr)
 {
-    void *newbrk;
+  void *newbrk;
 
-    {
-      register long int res __asm__("r2") = (long int)addr;
+  newbrk = (void *) INTERNAL_SYSCALL_NCS(__NR_brk,, 1, addr);
 
-      __asm__ volatile ("lrw  r1, %1\n\t"
-                        "trap 0\n\t"
-                        :"+r" (res)
-                        :"i" (__NR_brk) :"r1");
-      newbrk = (void *) res;
-    }
-    __curbrk = newbrk;
+  __curbrk = newbrk;
 
-    if (newbrk < addr)
-    {
-        __set_errno (ENOMEM);
-	return -1;
-    }
+  if (newbrk < addr)
+  {
+      __set_errno (ENOMEM);
+      return -1;
+  }
 
-    return 0;
+  return 0;
 }
 
 libc_hidden_def(brk)
 
-#endif
